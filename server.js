8415a1da1f55797742b4c2d2e57605ddb6fde93f
Jeferson Vieira Borges de Campos <jeffbcampos@gmail.com>
2023-10-02 20:24:29 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+express = require('express');
+
+const app = express();
+app.use(require('cors')());
+app.use(express.json());
+
+app.post('/send', (req, res, next) => {
+    const data = {...req.body};
+    require('./src/services/emailService/index')(data)
+        .then(response => res.json(response))
+        .catch(error => res.status(500).json(error));
+});
+
+
+app.listen(3000, () => {
+    console.log('start');
+});
